869d9fad4f723c2d202d5f73b9ba8175eb337693
Henrique Angar <devhenriqueangar@gmail.com>
2023-05-23 16:36:50 -0300

    Start me up

---
+++ Desktop/PESSOAL/3 - FACU/FRONTEND/QLQC/fazol/src/components/Form.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Form() {
+
+    return (
+        <div>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+    )
+}
